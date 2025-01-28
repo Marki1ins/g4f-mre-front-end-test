10a4391715d4994f314c8571ec8f80c1f963d7a7
Marki1ins <marcosdfcarvalhoo@gmail.com>
2025-01-27 23:29:45 -0300

    feat: :wrench: add docker files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4173,
+    open: false,
+    host: '0.0.0.0'
+  },
   plugins: [react()],
-})
+});
